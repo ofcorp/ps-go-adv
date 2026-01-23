47e5c329f969fd676d96a733f901f1da4b50e5d6
ofcorp <director@demrev.biz>
2026-01-23 23:41:58 +0300

    4-order-api dz1

---
+++ 4-order-api/migrations/auto.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"ps-go-adv/4-order-api/internal/product"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB_DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&product.Product{})
+}
